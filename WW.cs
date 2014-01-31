61e8c3690b8297aedde960f9d1240cba10dcc4fd
Kresimir Cvitanovic <kresimir.cvitanovic@gmail.com>
2014-01-30 17:16:53 -0800

    V0.2
    Added an editor window to help with sorting debug output.

--- WW.cs
+++ WW.cs
@@ -21,21 +21,24 @@
 //
 //  </Advanced console debugger for Unity 3D>
 // ------------------------------------------------------------------------------
+
 using UnityEngine;
 using System.Collections;
 
 namespace WW
 {
-
+	
 	
 	public static class debug
 	{
 		
 		//deactivate debugger to disable all log calls.
-		public static bool debuggerActive = true;
+		public static bool debuggerActive = false;
 		
 		//debugger will print only important stuff (lower level = more important)
-		public static int debugLevel = 0; // 0 - 9
+		public static int debugLevel = 0; // 0 - 9.
+		public static string importantGO = "";
+		public static int debugFlags = 0;
 		
 		//available log categories
 		public enum cat
@@ -50,11 +53,14 @@ namespace WW
 			ANIMATION = 7,
 			PLAYER = 8,
 			EDITOR = 9,
-			GAME = 10
+			GAME = 10,
+			UI = 11,
+			MONSTER = 12,
+			FOGOFWAR = 13
 		};
 		
 		//each category will have it's own color
-		private static string[] colors = new string[] {
+		public static string[] colors = new string[] {
 			"FF0000",//red(0)					-- IMPORTANT
 			"FF9933",//orange (1)				-- TRIGGERS
 			"669999",//dull green/blue  (2)		-- In game UI			
@@ -65,34 +71,80 @@ namespace WW
 			"D96EBF",//purple (7)
 			"66FFCC",//greenish (8)
 			"FFFFFF",//white; (9)				-- EDITOR
-			"666699"//dark purple; (10)
-		};
+			"666699",//dark purple; (10)
+			"AC5930", //brown-ish	(11)
+			"BAC74A", //dirty green
+			"E38B81" //washed up red
 
+		};
 		
 		
-		public static void log(string str)
+		
+		public static void log(object str)
 		{
 			log((int)cat.IMPORTANT, str, 0, null);
 		}
-		public static void log(int type, string str)
+		public static void log(object str, GameObject context)
+		{
+			log((int)cat.IMPORTANT, str, 0, context);
+		}
+
+		public static void log(int type, object str)
 		{
 			log(type, str, 0, null);
 		}
-		public static void log(int type, string str, float lvl)
+		public static void log(int type, object str, int lvl)
 		{
 			log(type, str, lvl, null);
 		}
-
+		
 
 		//format the string and print it out
-		public static void log(int type, string str, float lvl, GameObject context)
+		public static void log(int type, object str, int lvl, GameObject context)
 		{
+			//Debug.Log("Debugger settings:"+debuggerActive+", "+debugLevel+", "+importantGO+", "+debugFlags);
+
+			str = str.ToString();
+
 			if(!debuggerActive)
+			{	//using debugger through other than editor
+				Debug.Log(type + "> " + str);
 				return;
-			
-			if(lvl < debugLevel)
+			}
+
+			//are levels set?
+			int minLvl = debugLevel;
+			if(lvl > minLvl)
 				return;
+
+			//get object's name
+			string objInfo = " ";
+			if(context)
+			{
+				objInfo += context.transform.root.gameObject.name;
+				//if context is _fog_of_war, then it's obviously fog of war
+				if(objInfo == " _Fog of war")
+					type = (int)cat.FOGOFWAR;
+
+
+			}
 			
+			//color it
+			string myColor = colors[type];
+			//mark it as red (if this is important object)
+			if(context != null)
+			{
+				if(context.name == importantGO || context.transform.root.gameObject.name == importantGO)
+					myColor = colors[0];
+			}
+
+
+
+			//are flags on for this type?
+			int layer = 1 << type;
+			if ((debugFlags & layer) == 0)
+				return;
+
 			//create indentation depending on the debugLevel
 			string tabs = "";
 			for(int t=1; t<=lvl; t++)
@@ -101,35 +153,23 @@ namespace WW
 			}
 			
 			tabs += "-{"+lvl+"} ";
-
+			
 			//get category name as string
 			cat typeName = ((cat)type);
 			string typeNameString = typeName.ToString();
 
-			string objInfo = " ";
-			if(context)
-				objInfo += context.transform.root.gameObject.name;
 
 			//format
-			str = "<color=#" + colors[type] + ">" +
+			str = "<color=#" + myColor + ">" +
 				"["+typeNameString+"] " +
 					tabs +
 					"<b>" + str + "</b>" +
-					"<i>" + objInfo + "</i>" +
+					"<i>\t\t\t[" + objInfo + "]</i>" +
 					"</color>";
 			
 			//print it out
 			Debug.Log(str, context);
 		}
-		
-		
-		
-		
-		
-		
-		
-		
-		
+
 	}
 }
-
